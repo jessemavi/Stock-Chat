92e2ec570e0074bed860f27c274149c6596605b0
Jesse Mavi <jesse.mavi@gmail.com>
2017-12-17 14:19:06 -0600

    Adds apollo server with mock data

---
+++ server/index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
+
+// Some fake data
+const books = [
+  {
+    title: "Harry Potter and the Sorcerer's stone",
+    author: 'J.K. Rowling'
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton'
+  }
+];
+
+// The GraphQL schema in string form
+const typeDefs = `
+  type Query {
+    books: [Book] 
+  }
+
+  type Book { 
+    title: String 
+    author: String
+  }
+`;
+
+const resolvers = {
+  Query: { 
+    books: () => books 
+  }
+};
+
+// Put together a schema
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+
+const app = express();
+
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+
+app.listen(4000, () => {
+  console.log('Go to http://localhost:4000/graphiql to run queries!');
+});
