b2e1d7c86d9d6dbac69bfd9be3deedb71783b4b2
Jesse Mavi <jesse.mavi@gmail.com>
2018-01-05 19:11:27 -0600

    Updates LoggedInHeader to display username and includes link to user profile

--- client/src/LoggedInHeader.js
+++ client/src/LoggedInHeader.js
@@ -1,27 +1,57 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
 import { Menu, Container, Button, Grid, Popup } from 'semantic-ui-react';
 
+import client from './index';
+
 class LoggedInHeader extends Component {
-  onClick = () => {
+  state = {
+    user: ''
+  }
+
+  getUserQuery = gql`
+    {
+      getUser(user_id: ${JSON.parse(localStorage.getItem('user_id'))}) {
+        username
+      }
+    }
+  `;
+
+  componentDidMount = async () => {
+    const userResponse = await client.query({
+      query: this.getUserQuery
+    });
+    // console.log('userResponse', userResponse);
+    this.setState({
+      user: userResponse.data.getUser
+    });
+  }
+
+  onSignOutClick = () => {
+    console.log('onSignOutClick');
     localStorage.removeItem('token');
     localStorage.removeItem('user_id');
   }
 
   render() {
     return (
-      <Menu size='large'>
+      <Menu size='huge'>
         <Container>
           <Menu.Menu position='left'>
             <Menu.Item>Stock Chat</Menu.Item>
           </Menu.Menu>
           <Menu.Menu position='right'>
             <Menu.Item fitted='vertically'>
-              <Popup wide trigger={<Button content='Username' color='green' />} on='click'>
-                  <Grid divided columns='equal'>
-                    <Button content='Profile' basic color='green' fluid />
-                    <Button onClick={this.onClick} href='/login' content='Logout' basic color='green' fluid />
-                  </Grid>
-                </Popup>
+              <Popup trigger={<Button content={this.state.user.username} color='green' />} on='click' size='large'>
+                <Grid divided columns='equal' >
+                  <Grid.Row>
+                    <Menu.Item name='Profile' href='/profile'></Menu.Item>
+                  </Grid.Row>
+                  <Grid.Row>
+                    <Menu.Item name='Sign Out' onClick={this.onSignOutClick} href='login'></Menu.Item>
+                  </Grid.Row>
+                </Grid>
+              </Popup>
             </Menu.Item>
           </Menu.Menu>
         </Container>
